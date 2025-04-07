d0981d652ed9f628ca83309afb627d21d9dc1ab1
Fabongita <nashmpiii@gmail.com>
2025-04-07 13:27:11 +0800

    shuffled the options list using random.shuffle, then assigned the correct answer to the corresponding randomized label

--- quiz_maker_program.py
+++ quiz_maker_program.py
@@ -4,10 +4,14 @@ import tkinter as tk
 import random
 
 # Add main user question input logic
-option_list = ["a", "b", "c", "d"] #initializes the choices a b c and d
+option_label = ["a", "b", "c", "d"] #initializes the choices a b c and d
 flow_of_the_game = True # variable that checks if the user still wants to add questions or no
 while flow_of_the_game:
     question = input("Think of a multiple choice question and input it here: ") # asks users to add there question
     correct_answer = input("input the correct answer: ") #ask user to input the correct answer
-    wrong_answers = [int(input(f"input the incorrect answer 3 times ({i+1}): ")) for i in range(3)]
+    wrong_answers = [int(input(f"input the incorrect answer 3 times ({i+1}): ")) for i in range(3)] #asks the user to input the wrong answer 3 times
+    random.shuffle(option_label) #shuffles the options list
+    correct_label = option_label[0] #takes only one option from the list
+    correct_option = {correct_label: correct_answer}
     
+
