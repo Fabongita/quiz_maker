d6804b1c5c7391dca3f976a400d5f158a3e3bdcd
Fabongita <nashmpiii@gmail.com>
2025-04-07 12:43:25 +0800

    imported random module

--- quiz_maker_program.py
+++ quiz_maker_program.py
@@ -1,2 +1,6 @@
 # Import tkinker for the user interface  
 import tkinter as tk
+# Import random to make sure that the correct answer is at least randomly in a b c or d
+import random
+
+# Add main user question input logic
