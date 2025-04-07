a2fa752fa8d08f9c30368dacb1ba217359dca32d
Fabongita <nashmpiii@gmail.com>
2025-04-07 13:50:36 +0800

    added variable for the incorrect option then a for loop that iterates through the randomized options list using enumerate() then added the incorrect answers to the incorrect options dictionary

--- quiz_maker_program.py
+++ quiz_maker_program.py
@@ -4,14 +4,20 @@ import tkinter as tk
 import random
 
 # Add main user question input logic
-option_label = ["a", "b", "c", "d"] #initializes the choices a b c and d
 flow_of_the_game = True # variable that checks if the user still wants to add questions or no
+
 while flow_of_the_game:
+    option_label = ["a", "b", "c", "d"] #initializes the choices a b c and d which resets after the loop starts again
     question = input("Think of a multiple choice question and input it here: ") # asks users to add there question
     correct_answer = input("input the correct answer: ") #ask user to input the correct answer
     wrong_answers = [int(input(f"input the incorrect answer 3 times ({i+1}): ")) for i in range(3)] #asks the user to input the wrong answer 3 times
     random.shuffle(option_label) #shuffles the options list
     correct_label = option_label[0] #takes only one option from the list
-    correct_option = {correct_label: correct_answer}
-    
+    correct_option = {correct_label: correct_answer} #assigns the correct answer to the label
+    incorrect_option = {}
+    for index, labels in enumerate(option_label[1:]): # goes through the randomized option labels and assigns the index and the specific label
+        incorrect_option[labels] = wrong_answers[index]
+
+    if question:
+
 
