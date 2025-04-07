fd6e09dee731d2c11b818f056f815c1e6b3560ba
Fabongita <nashmpiii@gmail.com>
2025-04-07 12:59:02 +0800

    initialized input variables, added a while loop that checks if it should continue the game or not, inititialized a list for the options.

--- quiz_maker_program.py
+++ quiz_maker_program.py
@@ -4,3 +4,8 @@ import tkinter as tk
 import random
 
 # Add main user question input logic
+option_list = ["a", "b", "c", "d"] #initializes the choices a b c and d
+flow_of_the_game = "continue" # variable that checks if the user still wants to add questions or no
+while flow_of_the_game == "continue":
+    question = input("Think of a multiple choice question and input it here: ") # asks users to add there question
+    correct_answer = input("input the correct answer: ") #ask user to input the correct answer
