f42c4c4966111f736b18567f3b1ef2698781c493
Fabongita <nashmpiii@gmail.com>
2025-04-09 01:23:50 +0800

    replaced the inputs with an askstring function. put all of my code into one function so that I can use it as a command for the button

--- quiz_maker_program.py
+++ quiz_maker_program.py
@@ -14,45 +14,45 @@ question_list = [] #questions to be used
 
 # Add main user question input logic
 flow_of_the_game = True # variable that checks if the user still wants to add questions or no
+def questions_options_answers():
+    while flow_of_the_game:
+        option_label = ["a", "b", "c", "d"] #initializes the choices a b c and d which resets after the loop starts again
+        question = simpledialog.askstring("input question", "Think of a multiple choice question and input it here (enter nothing if you are done): ") # asks users to add there question, and add blank if they want to stop
+        
+        if not question: #checks if the input in the question variable is a space or not
+        
+            #add a file handling logic that collects the data from the questions list to a text file
+         with open("testing2_quizgame.txt", "w") as file:
+            for items in question_list: #iterates through the items inside the question list
+             file.write("Question: " + items["Questions"] + "\n") #specifically prints the question 
+            for label, options in sorted(items["Options"].items()): 
+                file.write(f"{label.upper()}  {options} \n") #prints the choices
+            for label, answers in(items["correct answer"].items()): #iterates through the items inside the correct answers list
+                file.write("correct answer " + label.upper() + ": " + answers + "\n") #Prints the correct answer inside the text file
 
-while flow_of_the_game:
-    option_label = ["a", "b", "c", "d"] #initializes the choices a b c and d which resets after the loop starts again
-    question = input("input question", "Think of a multiple choice question and input it here (enter nothing if you are done): ") # asks users to add there question, and add blank if they want to stop
-    
-    if not question: #checks if the input in the question variable is a space or not
-       
-        #add a file handling logic that collects the data from the questions list to a text file
-       with open("testing2_quizgame.txt", "w") as file:
-        for items in question_list: #iterates through the items inside the question list
-           file.write("Question: " + items["Questions"] + "\n") #specifically prints the question 
-           for label, options in sorted(items["Options"].items()): 
-              file.write(f"{label.upper()}  {options} \n") #prints the choices
-           for label, answers in(items["correct answer"].items()): #iterates through the items inside the correct answers list
-            file.write("correct answer " + label.upper() + ": " + answers + "\n") #Prints the correct answer inside the text file
+            break #breaks the whole loop
+        correct_answer = simpledialog.askstring("Input correct answer", "input the correct answer: ") #ask user to input the correct answer
 
-        break #breaks the whole loop
-    correct_answer = simpledialog.askstring("Input correct answer", "input the correct answer: ") #ask user to input the correct answer
+        wrong_answers = simpledialog.askstring[input(f"input the incorrect answer 3 times ({i+1}): ") for i in range(3)] #asks the user to input the wrong answer 3 times
+        
+        random.shuffle(option_label) #shuffles the options list
+        
+        correct_label = option_label[0] #takes only one option from the list
+        
+        correct_option = {correct_label: correct_answer} #assigns the correct answer to the label
+        
+        incorrect_option = {}
+        
+        for index, label in enumerate(option_label[1:]): # goes through the randomized option labels and assigns the index and the specific label
+            incorrect_option[label] = wrong_answers[index]
+        
+        all_options = {**correct_option, **incorrect_option} # merges all the options
 
-    wrong_answers = [input(f"input the incorrect answer 3 times ({i+1}): ") for i in range(3)] #asks the user to input the wrong answer 3 times
+        question_data = {"Questions": question,
+                        "Options": all_options,
+                        "correct answer": correct_option}
     
-    random.shuffle(option_label) #shuffles the options list
-    
-    correct_label = option_label[0] #takes only one option from the list
-    
-    correct_option = {correct_label: correct_answer} #assigns the correct answer to the label
-    
-    incorrect_option = {}
-    
-    for index, label in enumerate(option_label[1:]): # goes through the randomized option labels and assigns the index and the specific label
-        incorrect_option[label] = wrong_answers[index]
-    
-    all_options = {**correct_option, **incorrect_option} # merges all the options
-
-    question_data = {"Questions": question,
-                     "Options": all_options,
-                     "correct answer": correct_option}
-   
-    question_list.append(question_data)
+        question_list.append(question_data)
 
 
 
