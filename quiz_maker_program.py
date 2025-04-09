131d116b711780bee96353c04868bfe9fba4cca5
Fabongita <nashmpiii@gmail.com>
2025-04-09 21:28:22 +0800

    imported json and replaced the for loops for the text file with a simple json dump function

--- quiz_maker_program.py
+++ quiz_maker_program.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import ttk, simpledialog  #added simple dialogue for simple user interaction
 # Import random to make sure that the correct answer is at least randomly in a b c or d
 import random
+import json 
 
 # Setup main window
 root = Tk()
@@ -21,15 +22,10 @@ def questions_options_answers():
         if not question: #checks if the input in the question variable is a space or not
         
             #add a file handling logic that collects the data from the questions list to a text file
-         with open("questions_and_answers.txt", "w", encoding= "utf-8") as file:
-            for items in question_list: #iterates through the items inside the question list
-                file.write("Question: " + items["Questions"] + "\n") #specifically prints the question 
-                for label, options in sorted(items["Options"].items()): 
-                    file.write(f"{label.upper()}  {options} \n") #prints the choices
-                for label, answers in(items["correct answer"].items()): #iterates through the items inside the correct answers list
-                    file.write("correct answer " + label.upper() + ": " + answers + "\n") #Prints the correct answer inside the text file
-
+         with open("questions_and_answers.json", "w", encoding= "utf-8") as file:
+            json.dump(question_list, f, indent=4)
             break #breaks the whole loop
+        
         correct_answer = simpledialog.askstring("Input correct answer", "input the correct answer: ") #ask user to input the correct answer
 
         wrong_answers = []
