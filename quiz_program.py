41a073964c7f5d1060c7dd0c1ea516f909adfbf7
Fabongita <nashmpiii@gmail.com>
2025-05-01 20:15:09 +0800

    added variables for the questions answers and options

--- quiz_program.py
+++ quiz_program.py
@@ -5,5 +5,11 @@ import json
 # Unload all the contents of questions and answer JSON file
 with open("questions_and_answers.JSON", "r", encoding= "utf-8" ) as file:
     json_file = json.load(file)
-print(json_file)
+# Set the Questions , Options, Correct answer
+questions = (json_file["Questions"])
+options = (json_file["Options"])
+correct_answer = (json_file["correct answer"])
+             
 # Open the main window of the tkinter
+root = Tk()
+root.title("Quiz game")
