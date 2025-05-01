7eaf277138acf65647ea21a1af6aa3ac584eeb6b
Fabongita <nashmpiii@gmail.com>
2025-05-02 00:47:17 +0800

    added function with no values that the respective buttons will use

--- quiz_program.py
+++ quiz_program.py
@@ -2,6 +2,17 @@ from tkinter import *
 import random
 import json 
 
+# Function for the quiz making logic the quiz creator button is going to use
+def quiz_maker():
+    pass
+
+# Function for the when the start is pressed that the start button is going to use
+def start_button():
+    pass
+
+# Function for the saved quizzes the quiz saved quizzes button is going to use
+def saved_quizzes():
+    pass
 # Unload all the contents of questions and answer JSON file
 with open("questions_and_answers.JSON", "r", encoding= "utf-8" ) as file:
     json_file = json.load(file)
@@ -30,7 +41,7 @@ intro_label = Label(root, text=ascii_art, font="Courier")
 intro_label.pack(side="top", padx = "20", pady="20")
 
 # buttons
-quiz_creator_button = Button(root, text="sheesh bro", height="7", width="20",  activebackground="blue", activeforeground="yellow")
+quiz_creator_button = Button(root, text="sheesh bro", height="7", width="20",  activebackground="blue", activeforeground="yellow", command = quiz_maker )
 quiz_creator_button.pack(padx="20", pady="20", side="left") 
 start_button = None
 saved_quizzes_button = None
