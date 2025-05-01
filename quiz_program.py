d71a9133ec9fd767e1bbc1d56d33c4129faf3ffd
Fabongita <nashmpiii@gmail.com>
2025-05-01 17:50:24 +0800

    tried to use the quiz creator as reference to parse through the files

--- quiz_program.py
+++ quiz_program.py
@@ -3,4 +3,7 @@ import random
 import json 
 
 # Unload all the contents of questions and answer JSON file
+with open("questions_and_answers.JSON", "W", encoding= "utf-8" ) as file:
+    json_file = json.load(question_list, file, indent =4)
+print(json_file)
 # Open the main window of the tkinter
