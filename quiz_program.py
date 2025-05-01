e6788b6140499ea9db43fb4b7a8f5e8df00be1a2
Fabongita <nashmpiii@gmail.com>
2025-05-01 17:52:23 +0800

    fixed the parsing syntax

--- quiz_program.py
+++ quiz_program.py
@@ -3,7 +3,7 @@ import random
 import json 
 
 # Unload all the contents of questions and answer JSON file
-with open("questions_and_answers.JSON", "W", encoding= "utf-8" ) as file:
-    json_file = json.load(question_list, file, indent =4)
+with open("questions_and_answers.JSON", "r", encoding= "utf-8" ) as file:
+    json_file = json.load(file)
 print(json_file)
 # Open the main window of the tkinter
